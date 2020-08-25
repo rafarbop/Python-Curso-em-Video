e12d0e4174c2ee0fff4e6179c12f7dad0d3227c3
rafarbop <67751655+rafarbop@users.noreply.github.com>
2020-08-25 12:06:31 -0300

    Update aula09-exemplos.py

--- Python-Curso-em-Video/aula09-exemplos.py
+++ Python-Curso-em-Video/aula09-exemplos.py
@@ -6,15 +6,37 @@
 
 ''''''
 
-import emoji
-
 
 print('************************************************************')
 print('\nAula 04 - Exemplos e Testes')
 
+frase = input("Digite uma Frase para testar alguns métodos utilizados com Strings: ")
+print(f'A frase digitada possui {len(frase)} caracteres!')
+print()
+print(f"O primeiro caractere (frase[0]) é: ".ljust(50)+f"'{frase[0]}'")
+print(f"O quinto caractere (frase[4]) é: ".ljust(50)+f"'{frase[4]}'")
+print(f"Os primeiros 10 caracteres (frase[:10]) são: ".ljust(50)+f"'{frase[:10]}'")
+print("Caracteres 5º a 15º (frase[4:14]) são: ".ljust(50)+f"'{frase[4:14]}'")
+print()
+print(f"O último caractere (frase[-1]) é: ".ljust(50)+f"'{frase[-1]}'")
+print(f"Os últimos 10 caracteres (frase[-10:]) são: ".ljust(50)+f"'{frase[-10:]}'")
+print()
+print(f"10 caracteres iniciais pulando de 2 em 2 (frase[:10:2]) é: ".ljust(50)+f"'{frase[:10:2]}'")
 
 
-print(emoji.emojize("teste emoji :cry:",use_aliases=True))
+
+
+frase.count(' ')
+
+frase.strip()
+frase.lstrip()
+frase.rstrip()
+
+frase.split()
+' '.join(frase)
+
+
+
 print('\nFim da execução\n')
 print('************************************************************')
 
