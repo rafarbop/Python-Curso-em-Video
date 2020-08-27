c5ad156da237dc170627aa1b0204ffc98a64727c
rafarbop <67751655+rafarbop@users.noreply.github.com>
2020-08-27 18:12:29 -0300

    Update aula09-exemplos.py

--- aula09-exemplos.py
+++ aula09-exemplos.py
@@ -11,8 +11,8 @@ print('************************************************************')
 print('\nAula 04 - Exemplos e Testes')
 
 frase = input("Digite uma Frase para testar alguns métodos utilizados com Strings: ")
-print(f'A frase digitada possui {len(frase)} caracteres!')
-print()
+print(f'\nA frase digitada possui {len(frase)} caracteres!\n')
+
 print(f"O primeiro caractere (frase[0]) é: ".ljust(50)+f"'{frase[0]}'")
 print(f"O quinto caractere (frase[4]) é: ".ljust(50)+f"'{frase[4]}'")
 print(f"Os primeiros 10 caracteres (frase[:10]) são: ".ljust(50)+f"'{frase[:10]}'")
@@ -20,9 +20,9 @@ print("Caracteres 5º a 15º (frase[4:14]) são: ".ljust(50)+f"'{frase[4:14]}'")
 print()
 print(f"O último caractere (frase[-1]) é: ".ljust(50)+f"'{frase[-1]}'")
 print(f"Os últimos 10 caracteres (frase[-10:]) são: ".ljust(50)+f"'{frase[-10:]}'")
-print()
 print(f"10 caracteres iniciais pulando de 2 em 2 (frase[:10:2]) é: ".ljust(50)+f"'{frase[:10:2]}'")
-
+print()
+print(f"A quantidade de Espaços (frase.count(' ')) na frase digitada é: ".ljust(50)+f"'{frase.count(' ')}'")
 
 
 
