1ef94986e0b96d0b6e5dae59b5dd549e41674639
Rafa Bruno Paiva <rafarbop@gmail.com>
2020-10-03 15:21:31 -0300

    alteração nome variável

--- desafio022-aula09.py
+++ desafio022-aula09.py
@@ -10,14 +10,14 @@ print('\nDesafio 22')
 print('Este programa utiliza diversos métodos de strings para mostrar algumas informações.\n')
 
 print()
-nome = input('Digite seu nome completo: ')
+nome = str(input('Digite seu nome completo: ')).strip()
 print()
 
 print('Maiúsculo: '+nome.upper())
 print('Minúsculo: '+nome.lower())
 nome_sem_espaco = nome.replace(" ","")
 print(f'O nome digitado tem {len(nome_sem_espaco)} caracteres e {len(nome) - len(nome_sem_espaco)} espaços entre as palavras')
-primeira_palavra = nome.split()
-print(f'A primeira palavra ({primeira_palavra[0]}) possui {len(primeira_palavra[0])} caracteres')
+nome_separado = nome.split()
+print(f'A primeira palavra ({nome_separado[0]}) possui {len(nome_separado[0])} caracteres')
 
 print('\n---Fim da execução---\n')
