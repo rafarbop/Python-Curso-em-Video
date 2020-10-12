c983b38abe63e5f0852a1ea5cbc0b53b9c0711c3
Rafa Bruno Paiva <rafarbop@gmail.com>
2020-10-12 15:34:30 -0300

    Create aula11-exemplos.py

---
+++ aula11-exemplos.py
@@ -0,0 +1,25 @@
+#Aula 11 do Curso Python em Video!
+
+#By Rafabr
+
+import time,sys,subprocess
+
+subprocess.run(['clear'])
+print('\033[1;46;37m',end="")
+print('\n'+'*'*80)
+print('Aula 11 - Exemplos e Testes'.center(80)+'\n')
+print('\033[m',end="")
+print('\033[1;36m')
+
+
+print('TESTE'.center(80,"*"))
+
+
+print('\033[m')
+print('\033[7;41;37m',end="")
+print('\n'+'Fim da execução'.center(80))
+print('*'*80)
+print('\033[m',end="")
+time.sleep(2)
+
+
