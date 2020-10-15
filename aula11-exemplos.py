5c818393f755381b53e73d9ae507fd3b07ab0f5b
Rafa Bruno Paiva <rafarbop@gmail.com>
2020-10-15 14:24:11 -0300

    Update aula11-exemplos.py

--- aula11-exemplos.py
+++ aula11-exemplos.py
@@ -2,10 +2,10 @@
 
 #By Rafabr
 
-import time,sys,subprocess
+import time,sys,os
 
-subprocess.run(['clear'])
-print('\033[1;46;37m',end="")
+os.system('clear')
+print('\033[1;4;47;34m',end="")
 print('\n'+'*'*80)
 print('Aula 11 - Exemplos e Testes'.center(80)+'\n')
 print('\033[m',end="")
@@ -16,7 +16,7 @@ print('TESTE'.center(80,"*"))
 
 
 print('\033[m')
-print('\033[7;41;37m',end="")
+print('\033[1;41;37m',end="")
 print('\n'+'Fim da execução'.center(80))
 print('*'*80)
 print('\033[m',end="")
