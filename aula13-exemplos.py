cd6ed0dd5850219936b5c92321ee6c274a0a77c0
Rafa Bruno <rafarbop@gmail.com>
2020-12-15 22:55:11 -0300

    iniciada aula 13

---
+++ aula13-exemplos.py
@@ -0,0 +1,16 @@
+#Aula 13 do Curso Python em Video!
+
+#By Rafabr
+
+import time,sys
+import os
+from estrutura_modelo import cabecalho,rodape
+
+cabecalho(13,"Esta Aula será sobre Estruturas de Repetição FOR!")
+
+print('teste - nada')
+
+rodape()
+
+
+
