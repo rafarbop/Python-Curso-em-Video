d2bff3ea82bb6864dfdabdd740bb371d34356cd7
Rafa Bruno <67751655+rafarbop@users.noreply.github.com>
2021-01-16 11:08:19 -0300

    Update desafio021-aula08.py

--- desafio021-aula08.py
+++ desafio021-aula08.py
@@ -6,17 +6,25 @@
 
 import sys,time
 import playsound
-import pygame
+import pygame,replit
 
 print('\nDesafio 21')
 print('Este programa executa um arquivo de música MP3.\n')
 
 #playsound.playsound('a.mp3')
 
-pygame.init()
-pygame.mixer_music.load('a.mp3')
-pygame.mixer_music.play()
-time.sleep(10)
-pygame.mixer_music.stop
+source = replit.audio.play_file('a.mp3')
+
+if (source):
+  source
+  time.sleep(10)
+else:
+  pygame.init()
+  pygame.mixer_music.load(source)
+  pygame.mixer_music.play()
+  time.sleep(10)
+  pygame.mixer_music.stop
+
+
 
 print('\n---Fim da execução---\n')
