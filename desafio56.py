1f0679bcd7ffac68f5e2dc84389c907fe911c7dc
Rafa Bruno <rafarbop@gmail.com>
2021-03-15 14:57:29 -0300

    Update desafio56.py

--- desafio56.py
+++ desafio56.py
@@ -5,6 +5,7 @@
 from time import sleep
 from sys import exit
 from estrutura_modelo import cabecalho, rodape
+from statistics import fmean
 
 cabecalho(56, "Analisar dados - médio, maior, menor")
 
@@ -15,21 +16,36 @@ try:
         print('\nIniciando o cadastro de uma nova pessoa!')
         nome = input("Informe o nome da pessoa: ").strip()
         dicionario_pessoas[nome.split()[0]] = [nome]
-        dicionario_pessoas[nome.split()[0]] += [input('Informe a idade da pessoa: ')]
+        dicionario_pessoas[nome.split()[0]] += [int(input('Informe a idade da pessoa: '))]
         dicionario_pessoas[nome.split()[0]] += [input('Informe o sexo da pessoa:(M ou F) ')]
 except ValueError:
     print("Voçe digitou algum valor errado ou indevido!")
     sleep(1)
     exit()
 
+print()
+idades = [dicionario_pessoas[item][1] for item in dicionario_pessoas]
+
+dict_idade_crescente = sorted(dicionario_pessoas.items(), key=lambda x: x[1][1],reverse=True)
+
+lista_homem_velho = []
+
+for lista in dict_idade_crescente:
+    print(lista[1][2])
+    if lista[1][2] == 'M':
+        lista_homem_velho.append(lista)
+print()
+
+mulheres_menor20 = []
+
+for item in dicionario_pessoas:
+    if dicionario_pessoas[item][2] == 'F' and dicionario_pessoas[item][1] < 20:
+        mulheres_menor20.append(item)
 
 
-print(dicionario_pessoas)
-
-
-print(f"A média de idade das pessoas informadas é {0}")
-print(f"O homem mais velho se chama {'???'}")
-print(f"{0} mulheres possuem menos de 20 anos!")
+print(f"A média de idade das pessoas informadas é {fmean(idades):.2f}")
+print(f"O homem mais velho se chama {lista_homem_velho[0][0]}")
+print(f"{len(mulheres_menor20)} mulheres possuem menos de 20 anos!")
 
 
 rodape()
